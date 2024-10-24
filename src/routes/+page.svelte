215d3711ae376084ff1db888cd1a23988d285a72
Niko Pastore <nikopastore@gmail.com>
2024-10-23 19:23:58 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" class="home">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nikolai Pastore: Personal Site and Portfolio</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <h1>Nikolai Pastore</h1>
-    <img src="images/WLDI3061.JPG" alt="Photo of Nikolai Pastore" width="250">
-    <p>Welcome to my personal website and portfolio.  I am so excited to be in this course and can't wait to learn new tools and skills that will help me in my career.</p>
-
-    <script src="global.js" type="module"></script>
-</body>
-</html>
+<h1>Nikolai Pastore</h1>
+<img src="/images/WLDI3061.JPG" alt="Photo of Nikolai Pastore" width="250" />
+<p>
+  Welcome to my personal website and portfolio. I am so excited to be in this
+  course and can't wait to learn new tools and skills that will help me in my
+  career.
+</p>
