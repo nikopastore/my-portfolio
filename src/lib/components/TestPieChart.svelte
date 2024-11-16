491334f0cedb423ec14cb60b02c12b9aecbc956c
Niko Pastore <nikopastore@gmail.com>
2024-11-16 14:20:35 -0800

    Create TestPieChart.svelte

---
+++ src/lib/components/TestPieChart.svelte
@@ -0,0 +1,15 @@
+<!-- src/lib/components/TestPieChart.svelte -->
+<script>
+    import PieChart from './PieChart.svelte';
+  
+    let sampleData = [
+      { label: 'JavaScript', value: 30 },
+      { label: 'Python', value: 20 },
+      { label: 'Svelte', value: 15 },
+      { label: 'CSS', value: 10 },
+      { label: 'HTML', value: 25 }
+    ];
+  </script>
+  
+  <PieChart data={sampleData} width={300} height={300} innerRadius={50} outerRadius={100} />
+  
